4273d2b383b03dee868e313568b517f5617f7831
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-22 01:30:43 -0300

    Create users index for the api

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
+  resources :users, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
