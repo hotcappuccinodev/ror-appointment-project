b0dff09f820b18c271a418436b8101c6322753c2
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-24 01:47:05 -0300

    Fix linter errors

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::ReservationsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def create
-    @reservation = Reservation.create(user_id: params[:user_id], vehicle_id: params[:vehicle_id],city: params[:city],date: params[:date])
+    @reservation = Reservation.create(user_id: params[:user_id], vehicle_id: params[:vehicle_id], city: params[:city],
+                                      date: params[:date])
   end
 end
