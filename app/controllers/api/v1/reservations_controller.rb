fdb77cf1746bcb3162956119b6e08f382dffc313
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-24 01:41:32 -0300

    Add reservation#create action to reservation controller

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::ReservationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @reservation = Reservation.create(user_id: params[:user_id], vehicle_id: params[:vehicle_id],city: params[:city],date: params[:date])
+  end
+end
