ac3e446f257a8009525e446593d0adb2a534f90f
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-24 17:51:27 -0300

    Fix naming error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        registeration: 'users/registeration'
+        registerations: 'users/registerations'
       }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
