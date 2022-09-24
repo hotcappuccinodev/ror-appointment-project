46fd3f61cb11132712667871a65deab923f14e7b
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-24 02:48:42 -0300

    Create reservations#index action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::ReservationsController < ApplicationController
-  def index; end
+  def index
+    @reservations = Reservation.where(user_id: current_user.id)
+    render json: { status: 'Success', message: 'Loaded reservations', data: @reservations }, status: :ok
+  end
 
   def show; end
 
   def create
-    @reservation = Reservation.create(user_id: params[:user_id], vehicle_id: params[:vehicle_id], city: params[:city],
-                                      date: params[:date])
+    @reservation = Reservation.new(user_id: current_user.id, vehicle_id: params[:vehicle_id], city: params[:city],
+                                   date: params[:date])
+    if @reservation.save
+      render json: { status: 'Success', message: 'created reservation', data: @reservation },
+             status: :ok
+    else
+      render json: { status: 'Failed', message: 'Failed to create a new reservation', data: @reservations },
+             status: :expectation_failed
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
