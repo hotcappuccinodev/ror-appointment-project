eecbb1a544add895e3c2e1ae1d095fd19aacf03c
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-28 17:01:42 -0300

    Skip authentication for session controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  skip_before_filter :tokenized
 
   # GET /resource/sign_up
 
