ec0e3bdad6974503f4d491d345de43923a7e6661
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-29 08:25:21 +0530

    Fix linter

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::VehiclesController < ApplicationController
-  before_action :set_vehicle, only: [:show, :update, :destroy]
+  before_action :set_vehicle, only: %i[show update destroy]
 
   def index
     @vehicles = Vehicle.all
@@ -13,34 +13,35 @@ class Api::V1::VehiclesController < ApplicationController
   def create
     @vehicle = Vehicle.new(vehicle_params)
     if @vehicle.save
-      render json: {status: 'Success',message: 'Created Vehicle', data: @vehicle},status: :ok
+      render json: { status: 'Success', message: 'Created Vehicle', data: @vehicle }, status: :ok
     else
-      render json: {status: 'Failure', message: 'Could not create vehicle!', data: @vehicle},status: :unauthorized
+      render json: { status: 'Failure', message: 'Could not create vehicle!', data: @vehicle }, status: :unauthorized
     end
   end
 
   def update
     if @vehicle.update(vehicle_params)
-      render json: {status: 'Sucess', message: 'Updeted Vehicle Succesfully',data: @vehicle},status: :ok
+      render json: { status: 'Sucess', message: 'Updeted Vehicle Succesfully', data: @vehicle }, status: :ok
     else
-      render json: {status: 'Failure', errors: @vehicle.errors.full_messages, data: @vehicle}, status: :unauthorized
+      render json: { status: 'Failure', errors: @vehicle.errors.full_messages, data: @vehicle }, status: :unauthorized
     end
   end
 
   def destroy
     if @vehicle.destroy
-        render json: {status: 'Sucess', message: 'Deleted Vehicle Succesfully',data: @vehicle},status: :ok
+      render json: { status: 'Sucess', message: 'Deleted Vehicle Succesfully', data: @vehicle }, status: :ok
     else
-      render json: {status: 'Failure', errors: @vehicle.errors.full_messages, data: @vehicle}, status: :unauthorized
+      render json: { status: 'Failure', errors: @vehicle.errors.full_messages, data: @vehicle }, status: :unauthorized
     end
   end
 
   private
+
   def set_vehicle
     @vehicle = Vehicle.find_by(id: params[:id])
   end
 
   def vehicle_params
-    params.permit(:brand,:model,:price,:image,:description)
+    params.permit(:brand, :model, :price, :image, :description)
   end
 end
