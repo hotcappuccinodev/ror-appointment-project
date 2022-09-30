567312eac9488e6791306529374c5f9a01d7e6c0
ruikishimoto <ruikishimoto99@gmail.com>
2022-09-30 08:15:48 +0530

    Add reservation intergration test

--- spec/requests/reservation_spec.rb
+++ spec/requests/reservation_spec.rb
@@ -7,5 +7,32 @@ RSpec.describe '/api/v1/reservation', type: :request do
       User.first.confirm
       get api_v1_reservations_path,params:{authentication_token: User.first.authentication_token}
     end
+
+     it 'List reservation as authorized user' do
+      data = JSON.parse(response.body)
+      expect(data['status']).to eq('Success')
+    end
+
+     it 'Create reservation as authorized user' do
+      post api_v1_vehicle_reservations_path(Vehicle.first.id),params:{authentication_token: User.first.authentication_token, date: '24-09-2022', city: 'bravos'}
+      data = JSON.parse(response.body)
+      expect(data['status']).to eq('Success')
+    end
+
+     it 'Create reservation as invalid atrributs' do
+      post api_v1_vehicle_reservations_path(Vehicle.first.id),params:{authentication_token: User.first.authentication_token, date: '24-09-2022'}
+      data = JSON.parse(response.body)
+      expect(data['status']).to eq('Failed')
+    end
+  end
+   context 'Unauthorized access' do
+     before(:each) do
+      Rails.application.load_seed
+      User.first.confirm
+      get api_v1_reservations_path
+    end
+        it 'Respnse unauthrozed' do
+          expect(response.body).to include('You need to sign in or sign up before continuing')
+        end
   end
 end
