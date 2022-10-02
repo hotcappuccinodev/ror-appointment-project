fa90ef17ac199635d774e9f60ba1394c7f66b7b8
ruikishimoto <ruikishimoto99@gmail.com>
2022-10-02 09:00:37 +0530

    Downgrade bundler to 2.3.7

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# user1 = User.create!(name: 'John Doe', email: 'john123@gmail.com', password: 'test123', roles: ['admin'])
-# user2 = User.create!(name: 'Jane Doe', email: 'jane123@gmail.com', password: 'test123', roles: ['admin'])
-# user1.confirm
-# user2.confirm
-# veh1 = Vehicle.create!(brand: 'BWM', model: 'BMW-30', image: 'image-bmw', price: 80, description: 'good and lightspeed with low consumptiom')
-# veh2 = Vehicle.create!(brand: 'Macendiz', model: 'Benz-30', image: 'image-Benz', price: 90, description: 'good and lightspeed with low consumptiom')
-# veh3 = Vehicle.create!(brand: 'G-Wagon', model: 'G-wagon-15', image: 'image-wagon', price: 95, description: 'good and lightspeed with low consumptiom')
-# res1 = Reservation.create!(user: user1, vehicle: veh1, date: '24-09-2022', city: 'bravos')
-# res1 = Reservation.create!(user: user2, vehicle: veh1,date: '24-09-2022', city: 'capetown')
-# res1 = Reservation.create!(user: user2, vehicle: veh3,date: '24-09-2022', city: 'lusaka')
-# res1 = Reservation.create!(user: user1, vehicle: veh2,date: '24-09-2022', city: 'venice')
+user1 = User.create!(name: 'John Doe', email: 'john123@gmail.com', password: 'test123', roles: ['admin'])
+user2 = User.create!(name: 'Jane Doe', email: 'jane123@gmail.com', password: 'test123', roles: ['admin'])
+user1.confirm
+user2.confirm
+veh1 = Vehicle.create!(brand: 'BWM', model: 'BMW-30', image: 'image-bmw', price: 80, description: 'good and lightspeed with low consumptiom')
+veh2 = Vehicle.create!(brand: 'Macendiz', model: 'Benz-30', image: 'image-Benz', price: 90, description: 'good and lightspeed with low consumptiom')
+veh3 = Vehicle.create!(brand: 'G-Wagon', model: 'G-wagon-15', image: 'image-wagon', price: 95, description: 'good and lightspeed with low consumptiom')
+res1 = Reservation.create!(user: user1, vehicle: veh1, date: '24-09-2022', city: 'bravos')
+res1 = Reservation.create!(user: user2, vehicle: veh1,date: '24-09-2022', city: 'capetown')
+res1 = Reservation.create!(user: user2, vehicle: veh3,date: '24-09-2022', city: 'lusaka')
+res1 = Reservation.create!(user: user1, vehicle: veh2,date: '24-09-2022', city: 'venice')
