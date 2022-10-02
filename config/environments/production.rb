66c3d4f14b225e2c2e39417c22d7bb44754b42d6
ruikishimoto <ruikishimoto99@gmail.com>
2022-10-02 07:11:20 +0530

    Update prodoction env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "appointment_diary_backend_production"
-
+  config.action_mailer.default_url_options = { host: 'book-vehicle.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
