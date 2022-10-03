5552849af2ee1a8a7674c6982e890f085225b44f
ruikishimoto <ruikishimoto99@gmail.com>
2022-10-03 22:08:50 +0530

    Update Swagger to single component

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-
   def index
     @users = User.all
     render json: { status: 'Success', message: 'Loaded users', data: @users }, status: :ok
